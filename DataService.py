aac8ccb5a91db07734966a15139880af090ff9b5
jppianta <joao.pianta@outlook.com>
2020-04-20 21:25:16 -0300

    dataService commit

---
+++ DataService.py
@@ -0,0 +1,193 @@
+import pandas as pd
+import numpy as np
+
+jhu_opt = {"N": 1, "only_contiguous": True, "max_date": "last"}
+brasil_opt = {"N": 1, "max_date": "last"}
+
+
+class DataService:
+  __instance = None
+
+  @staticmethod
+  def getInstance(jhu_options=jhu_opt, brasil_options=brasil_opt):
+    """Returns instance of DataService.
+    This implements a Singleton pattern to make sure that the same instance of this class will be used everywhere
+    
+    Returns:
+        {DataService} -- DataService instance
+    """
+
+    if DataService.__instance == None:
+      DataService(jhu_options, brasil_options)
+    return DataService.__instance
+
+  def __init__(self, jhu_options, brasil_options):
+    """ Virtually private constructor. """
+    if DataService.__instance != None:
+      raise Exception("This class is a singleton!")
+    else:
+      self.jhu_data = None
+      self.jhu_metadata = dict()
+      self.brasil_io_data = None
+      self.brasil_io_metadata = dict()
+      self.start_date_columns = {"jhu": 5, "brasil_io": 3}
+      self.jhu_options = jhu_options
+      self.brasil_options = brasil_options
+      DataService.__instance = self
+
+  def get_jhu_data(self, force=False):
+    """Get data from JHU and save locally
+    
+    Keyword Arguments:
+        force {bool} -- If true, get data even if its cached (default: {False})
+    
+    Returns:
+        {DataFrame} -- Data from JHU
+    """
+    if not self.jhu_data or force:
+      jhu_url = "https://raw.githubusercontent.com/CSSEGISandData/"\
+          "COVID-19/master/csse_covid_19_data/"\
+          "csse_covid_19_time_series/"\
+          "time_series_covid19_confirmed_US.csv"
+
+      self.jhu_data = pd.read_csv(jhu_url)
+    return self.jhu_data.copy()
+
+  def get_brasil_io_data(self, force=False):
+    """Get data from brasil.io and save locally
+    
+    Keyword Arguments:
+        force {bool} -- If true, get data even if its cached (default: {False})
+    
+    Returns:
+        {DataFrame} -- Data from brasil.io
+    """
+
+    if not self.brasil_io_data or force:
+      brasil_io_url = "https://brasil.io/dataset/covid19/caso/?place_type=city&format=csv"
+
+      self.brasil_io_data = pd.read_csv(brasil_io_url)
+    return self.brasil_io_data.copy()
+
+  def __filter_N_max_data(self, source, data, N, max_date):
+    """Filters DataFrama from source by N and max_date
+    
+    Arguments:
+        source {str} -- Source of data. Either 'jhu' or 'brasil_io'
+        data {DataFrame} -- DataFrame to filter
+        N {int} -- Minimal number of cases
+        max_date {str} -- If default, return cases from all dates. If defined, return cases until defined date. Format m/d/yy (default: {"last"})
+    
+    Returns:
+        {DataFrame} -- Filtered DataFrame
+    """
+
+    metadata = self.jhu_metadata if source == "jhu" else self.brasil_io_metadata
+    start_date_columns = self.start_date_columns[source]
+    metadata["date_cols"] = list(data.columns)
+
+    if max_date == "last":
+      metadata["date_cols"] = metadata["date_cols"][start_date_columns:]
+    else:
+      metadata["date_cols"] = metadata["date_cols"][start_date_columns:data.
+                                                    columns.get_loc(max_date)]
+
+    data = data.sort_values(by=metadata["date_cols"],
+                            ascending=np.zeros(len(metadata["date_cols"]),
+                                               dtype=bool))
+
+    nfi = []
+    metadata["uid_positive"] = []
+    for uid in data['UID'].tolist():
+      n_infection = next(
+        (i for i, x in enumerate(data.loc[data['UID'] == uid,
+                                          metadata["date_cols"]].values[0])
+         if x >= N), "None")
+      if n_infection != "None":
+        metadata["uid_positive"].append(uid)
+      nfi.append(n_infection)
+
+    data.insert(start_date_columns, "Day_First_N_Infections", nfi)
+    return data
+
+  def get_filtered_brasil_io_data(self, options=None):
+    """Get data from Brasil.io and filters it using the arguments on the same format as JHU data
+    
+    Keyword Arguments:
+        options {BrasilOptions} -- Options to be used in the filter
+    
+    Returns:
+        {DataFrame} -- Brasil DataFrame filtered by arguments
+    """
+    options = options if options != None else self.brasil_options
+    N = options["N"]
+    max_date = options["max_date"]
+
+    data = self.get_brasil_io_data()
+    data = data[data.city != "Importados/Indefinidos"]
+
+    city_names = data["city"].unique().tolist()
+    dates = data["date"].unique().tolist()
+
+    ufs = []
+    uids = []
+    for city in city_names:
+      ufs.append(data.loc[data["city"] == city, "state"].values[0])
+      uids.append(data.loc[data["city"] == city, "city_ibge_code"].values[0])
+
+    # Usa o mesmo nome de colunas do arquivo da JHU por simplicidade
+    new_data = pd.DataFrame(data=uids, columns=["UID"])
+
+    new_data["city"] = city_names
+    new_data["Province_State"] = ufs
+
+    for date in dates:
+      daily_data = data.loc[data["date"] == date]
+      codes = daily_data["city_ibge_code"].tolist()
+      new_data[date] = np.zeros(len(uids))
+
+      for code in codes:
+        if code in uids:
+          i = new_data.index[new_data["UID"] == code].values[0]
+          new_data.at[i, date] = daily_data.loc[daily_data["city_ibge_code"] ==
+                                                code, "confirmed"].values[0]
+
+    return self.__filter_N_max_data('brasil_io', new_data, N, max_date)
+
+  def get_filtered_jhu_data(self, options=None):
+    """Get data from JHU and filters it using the arguments
+    
+    Keyword Arguments:
+        options {JhuOptions} Options to be used in the filter
+    
+    Returns:
+        {DataFrame} -- JHU DataFrame filtered by arguments
+    """
+    options = options if options != None else self.jhu_options
+    N = options["N"]
+    only_contiguous = options["only_contiguous"]
+    max_date = options["max_date"]
+
+    non_contiguous = [
+      "American Samoa", "Guam", "Northern Mariana Islands", "Puerto Rico",
+      "Virgin Islands", "Hawaii", "Alaska"
+    ]
+
+    data = self.get_jhu_data()
+    data = data.drop(columns=['iso2', 'iso3', 'code3', 'FIPS', 'Lat', 'Long_'])
+    if only_contiguous:
+      data = data.loc[~data['Province_State'].isin(non_contiguous)]
+
+    return self.__filter_N_max_data('jhu', data, N, max_date)
+
+  def save_to_file(self, data, out_file="output.csv"):
+    """Save DataFrame to csv file
+    
+    Arguments:
+        data {DataFrame} -- DataFrame to be save as a csv file
+    
+    Keyword Arguments:
+        out_file {str} -- Output file name (default: {"output.csv"})
+    """
+
+    data.to_csv(out_file)
--- TimeSeries.py
+++ TimeSeries.py
@@ -3,198 +3,69 @@
 Created on Sat Apr 18 14:41:44 2020
 
 """
-import pandas as pd
-import numpy as np
-import urllib as ul
-import gzip
 import matplotlib.pyplot as mp
+from DataService import DataService
+
 
 class TimeSeries:
-    __NON_CONTIGUOUS_US = [
-        "American Samoa", "Guam", "Northern Mariana Islands",\
-        "Puerto Rico", "Virgin Islands", "Hawaii", "Alaska"]
+  def __init__(self):
+    self.data_service = DataService.getInstance()
 
-    __JHU = "https://raw.githubusercontent.com/CSSEGISandData/"\
-        "COVID-19/master/csse_covid_19_data/"\
-        "csse_covid_19_time_series/"\
-        "time_series_covid19_confirmed_US.csv"
+  def __hist_first_infections(self, data, out_file, x_label, title, divs):
+    """Creates a histogram of the provided data
+    
+    Arguments:
+        data {DataFrame} -- DataFrame to be used on the histogram
+        out_file {str} -- Name of the image file
+        x_label {str} -- Label for the X axis
+        title {str} -- Title of the plot
+        divs {int} -- Size of histogram bins
+    """
+    fig, plot = mp.subplots(1, 1, figsize=(10, 10), constrained_layout=True)
 
-    __BR_IO = "https://data.brasil.io/dataset/covid19/caso_full.csv.gz"
+    filtered_data = data[data.Day_First_N_Infections != "None"]
+    plot.hist(filtered_data["Day_First_N_Infections"].values, bins=divs)
+    mp.xlabel(x_label, fontsize=18)
+    mp.ylabel("Frequency", fontsize=18)
+    mp.title(title, fontsize=22)
+    fig.savefig(out_file)
 
-    def __init__(
-            self, N, territory, raw_data="standard", only_contiguous=True,
-            max_date="last"):
-        """ Trata a serie temporal de casos por unidade territorial (condados ou municipios).
+  def brasil_hist_first_infections(self,
+                                   out_file="brasil_history",
+                                   x_label="",
+                                   title="",
+                                   divs=45,
+                                   options=None):
+    """Creates a histogram of Brasil
+    
+    Keyword Arguments:
+        out_file {str} -- Name of the image file (default: {"brasil_history"})
+        x_label {str} -- Label for the X axis (default: {""})
+        title {str} -- Title of the plot (default: {""})
+        divs {int} -- Size of histogram bins (default: {45})
+        options {BrasilOptions} -- Options to be used to filter data
+    """
 
-        Parameters
-        ----------
-        N : inteiro positivo
-            Numero de casos inicial minimo
-        territory: string
-            Indica a origem do arquivo, ou filtros a serem aplicados ao dataset
-            de acordo com a necessidade
-        raw_data : string (url ou path), optional
-            Se nao definido, usa o respositorio online da JHU
-        only_contiguous : booleano, optional
-            Se falso, considera os territorios nao continentais dos EUA, e o
-            Alaska
-        max_date : string (data formato americano - m/d/aa), opcional
-            Se padrao, recupera ate a ultima data do arquivo. Se definido,
-            recupera os dados ate a data informada
+    data = self.data_service.get_filtered_brasil_io_data(options)
 
-        Returns
-        -------
-        None.
+    self.__hist_first_infections(data, out_file, x_label, title, divs)
 
-        """
-        self.territory = territory
-        self.N = N
+  def jhu_hist_first_infections(self,
+                                out_file="jhu_history",
+                                x_label="",
+                                title="",
+                                divs=45,
+                                options=None):
+    """Creates a histogram of JHU Data
+    
+    Keyword Arguments:
+        out_file {str} -- Name of the image file (default: {"brasil_history"})
+        x_label {str} -- Label for the X axis (default: {""})
+        title {str} -- Title of the plot (default: {""})
+        divs {int} -- Size of histogram bins (default: {45})
+        options {JhuOptions} -- Options to be used to filter data
+    """
 
-        if (raw_data == "standard"):
-            self.get_online_dataset(territory)
-        else:
-            self.raw_data = raw_data
+    data = self.data_service.get_filtered_jhu_data(options)
 
-        if territory == "US":
-           # Remove colunas nao usadas
-           self.data = pd.read_csv(self.raw_data)\
-               .drop(columns=['iso2', 'iso3', 'code3', 'FIPS', 'Lat', 'Long_'])
-
-           if only_contiguous:
-              self.data = self.data.loc[
-                  ~self.data['Province_State'].isin(self.__NON_CONTIGUOUS_US)]
-
-           startdate_cols = 5
-           self.out_file_name = "_counties_diff_US.csv"
-        if territory == "BR":
-            self.data = pd.read_csv(self.raw_data)
-            startdate_cols = 3
-            self.out_file_name = "_municipios_dif_BR.csv"
-
-        # Colunas temporais
-        self.date_cols = list(self.data.columns)
-        if max_date == "last":
-            self.date_cols = self.date_cols[startdate_cols:]
-        else:
-            self.date_cols = self.date_cols[
-                startdate_cols:self.data.columns.get_loc(max_date)]
-
-        # Remove os totais de casos menores que N para a ordenacao
-        num_data = self.data._get_numeric_data()
-        num_data[num_data < N] = 0
-
-        # Ordena conforme os primeiros casos aparecem
-        self.data = self.data.sort_values(by=self.date_cols,
-                            ascending=np.zeros(
-                                len(self.date_cols), dtype=bool))
-
-        # Encontra o primeiro dia em que o condado atinge N casos
-        # e remove da analise os condados sem casos confirmados
-        nfi = []
-        self.uid_positive = []
-        for uid in list(x for x in self.data['UID']):
-            n_infection = next((i for i, x in enumerate(
-                self.data.loc[
-                    self.data['UID'] == uid,
-                    self.date_cols].values[0]) if x >= N), "None")
-            if n_infection != "None":
-                self.uid_positive.append(uid)
-            nfi.append(n_infection)
-
-        self.data.insert(self.startdate_cols, "Day_First_N_Infections", nfi)
-
-    def save_data_file(self, path=""):
-        """ Salva os dados com a coluna de dias ate a infeccao N apos outbreak
-        """
-        self.data.to_csv(
-            path +
-            "time_series_" +
-            str(self.N) + "_" +
-            self.territory + "_" +
-            self.date_cols[-1] + "_"
-            ".csv", index=False)
-
-    def get_online_dataset(self, territory):
-        """ Recupera os arquivos atualizados para um dado territorio
-        """
-        if territory == "US":
-            url = self.__JHU
-        elif territory == "BR":
-            url = self.__BR_IO
-
-        file = "data/" + url.split("/")[-1]
-        ul.request(url, file)
-        if ".gz" in url:
-            file_csv = open(file.replace(".gz", ""), "wb")
-            with gzip.open(file, "rb") as fgz:
-                content = fgz.read()
-            file_csv.write(content)
-            file_csv.close()
-        
-        if territory == "BR":
-            data = self._gen_table_br(file)
-            file = "data/time_series_covid19_confirmed_BR.csv"
-            data.to_csv(file)
-
-        self.raw_data = file
-
-    def hist_first_infections(self, out_file, x_label, title, divs=45):
-        """ Salva um histograma da distribuicao de primeiros N casos desde outbreak
-        """
-        fig, plot = mp.subplots(
-            1, 1, figsize=(10, 10), constrained_layout=True)
-
-        filtered_data = self.data[self.data.Day_First_N_Infections != "None"]
-        plot.hist(filtered_data["Day_First_N_Infections"].values, bins=divs)
-        mp.xlabel(x_label, fontsize=18)
-        mp.ylabel("Frequency", fontsize=18)
-        mp.title(title, fontsize=22)
-        fig.savefig(out_file)
-        
-    def _gen_table_br(self, raw):
-        """Gera uma tabela JHU-like para a serie temporal de casos de covid-19 no Brasil.
-
-        Parameters
-        ----------
-        raw : string, optional
-            Caminho para o arquivo original. The default is "caso_full.csv".
-            
-        Returns
-        -------
-        None. Salva um arquivo em disco com a serie temporal.
-
-        """
-        io_data = pd.read_csv(raw)
-
-        io_data = io_data[io_data.city != "Importados/Indefinidos"]
-
-        cities_codes = io_data["city_ibge_code"].unique().tolist()
-        dates = io_data["date"].unique().tolist()
-
-        # Usa o mesmo nome de colunas do arquivo da JHU por simplicidade
-        new_data = pd.DataFrame(data=cities_codes, columns=["UID"])
-
-        cities_names = []
-        ufs = []
-        for cco in cities_codes:
-            name = io_data.loc[io_data["city_ibge_code"] == cco, "city"].values[0]
-            state = io_data.loc[io_data["city_ibge_code"] == cco, "state"].values[0]
-            cities_names.append(name)
-            ufs.append(state)
-
-        new_data["city"] = cities_names
-        new_data["Province_State"] = ufs
-
-        for date in dates:
-            daily_data = io_data.loc[io_data["date"] == date]
-            codes = daily_data["city_ibge_code"].tolist()
-            new_data[date] = np.zeros(len(cities_codes))
-
-            for code in codes:
-                i = new_data.index[new_data["UID"] == code].values[0]
-                new_data.at[i, date] = daily_data.loc[
-                    daily_data["city_ibge_code"] == code,
-                    "last_available_confirmed"].values[0]
-        
-        return(new_data)
-        new_data.to_csv("time_series_confirmed_BR.csv", index=False)
+    self.__hist_first_infections(data, out_file, x_label, title, divs)
